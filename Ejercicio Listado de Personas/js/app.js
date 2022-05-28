b8bc85a906b9d1520d8a146018f30ea27797d966
Jerónimo <104911337+jserronimo@users.noreply.github.com>
2022-05-28 09:28:28 -0300

    Ejercicio Listado de Personas
    Aplicación web que permite hacer una lista de personas con los datos Nombre y Apellido. Si no se completan los campos la aplicación envía un error al usuario.
    Consiste en un archivo html, una hoja de estilos css, y un archivo app.js + un subarchivo .js

---
+++ Ejercicio Listado de Personas/js/app.js
@@ -0,0 +1,31 @@
+console.log("hola");
+
+const personas = [
+    new Persona("Juan","Perez"),
+    new Persona("Carla", "Suarez"),
+];
+
+function mostrarPersonas(){
+    console.log("mostrar personas");
+    let texto="";
+    for(let persona of personas){
+        console.log(persona);
+        texto +=`<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms["forma"];
+    const nombre = forma["nombre"];
+    const apellido = forma["apellido"];
+    if (nombre.value !="" && apellido.value !=""){
+            const persona = new Persona(nombre.value, apellido.value);
+            console.log(persona);
+            personas.push(persona);
+            mostrarPersonas();
+    }
+    else{
+        alert("Error: complete los campos de nombre y apellido")
+    }
+}
